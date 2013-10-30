17997afbb6f603f6240aea65d7fd881b92e13d63
Christian Mittendorf <christian.mittendorf@freenet.de>
2013-10-30 08:31:18 +0100

    Initial Commit

---
+++ TableViewFooterResize/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TableViewFooterResize
+//
+//  Created by Christian Mittendorf on 30.10.13.
+//  Copyright (c) 2013 Christian Mittendorf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TableViewFooterResize/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TableViewFooterResize
+//
+//  Created by Christian Mittendorf on 30.10.13.
+//  Copyright (c) 2013 Christian Mittendorf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
